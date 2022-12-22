766e06853154b9a0711aea9776ba84496abfa3ff
ghostbhd <bhd.a@yandex.com>
2022-12-23 00:21:23 +0100

    cub parssing v2.3

--- src/parse.c
+++ src/parse.c
@@ -6,11 +6,23 @@
 /*   By: abouhmad <abouhmad@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/22 23:46:02 by abouhmad          #+#    #+#             */
-/*   Updated: 2022/12/23 00:14:15 by abouhmad         ###   ########.fr       */
+/*   Updated: 2022/12/23 00:20:46 by abouhmad         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/header.h"
+
+char	*get_text(char *line)
+{
+	char	*text;
+	int		i;
+
+	i = 0;
+	if (line[i] == ' ')
+		ft_error();
+	text = ft_strdup(line);
+	return (text);
+}
 
 void	ft_parse(int fd, t_map **map)
 {
@@ -22,26 +34,20 @@ void	ft_parse(int fd, t_map **map)
 	line = get_next_line(fd);
 	while (line)
 	{
-		if (ft_strncmp(line, "R ", 2) == 0)
-			i++;
-		else if (ft_strncmp(line, "NO ", 3) == 0)
-			i++;
+		if (ft_strncmp(line, "NO ", 3) == 0)
+			(*map)->no = get_text(line + 3);
 		else if (ft_strncmp(line, "SO ", 3) == 0)
-			i++;
+			(*map)->so = get_text(line + 3);
 		else if (ft_strncmp(line, "WE ", 3) == 0)
-			i++;
+			(*map)->we = get_text(line + 3);
 		else if (ft_strncmp(line, "EA ", 3) == 0)
-			i++;
-		else if (ft_strncmp(line, "S ", 2) == 0)
-			i++;
+			(*map)->ea = get_text(line + 3);
 		else if (ft_strncmp(line, "F ", 2) == 0)
-			i++;
+			(*map)->f = get_text(line + 2);
 		else if (ft_strncmp(line, "C ", 2) == 0)
-			i++;
+			(*map)->c = get_text(line + 2);
 		else if (ft_strncmp(line, "1", 1) == 0)
-			i++;
-		else if (ft_strncmp(line, " ", 1) == 0)
-			i++;
+			return ;
 		else
 			ft_error();
 		line = get_next_line(fd);
